e8c6477197a7dace7b21e1a90be07cdcdf1c6207
Carsten Falk <carsten.falk@gmail.com>
2021-02-05 23:02:54 +0000

    Tailwind: production build, Preflight enabled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './dist/**/*.html',
+     './dist/**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
-  },
-  corePlugins: {
-     preflight: false,
   },
   plugins: [],
 }
