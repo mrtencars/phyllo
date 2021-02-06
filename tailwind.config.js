4157810a759f3cafed727a99da5cf38d41c61ca3
Carsten Falk <carsten.falk@gmail.com>
2021-02-06 16:50:46 +0100

    Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        aufgabe: '#345341'
+      }
+    },
   },
   variants: {
     extend: {},
